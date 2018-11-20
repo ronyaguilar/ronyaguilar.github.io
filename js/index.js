f25735b0cf2c835ac5defcb24d6652c2c8fd8b0f
Rony Aguilar <rony.aguilar09@gmail.com>
2018-11-19 17:32:04 -0800

    Created nav and js sticky

--- js/index.js
+++ js/index.js
@@ -1,22 +1,57 @@
 $(document).ready(() => {
+  let windowSize = $(window).height();
+  
+  let isIE = navigator.userAgent.search("MSIE") & gt; = 0
 
-  $('.toggable-title').click((e) => {
-    let $container = $(e.currentTarget).closest('.content-item');
-    let $title = $container.find('.toggable-title');
-    if($title.hasClass('active')){
-      $container.find('.collapse-wrapper').animate({
-        height: ["toggle"],
-        marginLeft: "-30px",
-        opacity: "toggle"
-      },800);
-      $title.removeClass('active');
-    }else{
-      $title.addClass('active');
-      $container.find('.collapse-wrapper').animate({
-        height: ["toggle"],
-        marginLeft: "0px",
-        opacity: "toggle"
-      },800);
-    }
+  // Edge 20+
+  let isEdge = !isIE && !!window.StyleMedia;
+  
+  
+  let resizeSection = () => {
+     // Set height for welcome section
+    $('#intro-container').height(windowSize);
+    $('#about').height(windowSize);
+  };
+  
+   resizeSection();
+    
+  //Resize the height for welcome section if screen changes
+  $(window).resize(() => {
+    windowSize = $(window).height();
+    resizeSection();
   });
+  
+  $('#explore-button').click(() => {
+      $('#main-wrapper').animate({
+        scrollTop: windowSize
+      }, 2000);
+  });
+  
+  // Change active class for nav a tags
+  $('.nav-title').click((e)=>{
+    console.log('nav title clicked')
+    let $nav_container = $('#nav');
+    $nav_container.find('.active').removeClass('active');
+    $(e.currentTarget).addClass('active');
+  });
+  
+  // Make nav fixed once past intro
+  if(isIE || isEdge){
+    $('#main-wrapper').scroll(()=> {
+    let container_width = $("#main-wrapper").width();        
+    let offset = $('#about').offset().top - 150;
+    let $nav = $('#nav');
+    let top = $('#main-wrapper').offset().top;
+    if(top >= offset){
+      console.log('nav now at top');
+      $nav.addClass('fixed');
+      $nav.width(container_width);
+    }
+    if(top < offset) {
+      console.log('nav not at top');
+      $nav.removeClass('fixed');
+    }
+    console.log('top: ' + top + ' offset: ' + offset);
+  });
+  }
 });
